81bdb126cf59600cb76fbaff290f87c5bd69f6f3
Josha Inglis <jinglis@iix.net>
2016-04-20 15:21:27 +1000

    Prevent Jira from logging user out of web client

--- bugwarrior/services/jira.py
+++ bugwarrior/services/jira.py
@@ -2,10 +2,33 @@ from __future__ import absolute_import
 
 import six
 from jinja2 import Template
-from jira.client import JIRA
+from jira.client import JIRA as BaseJIRA
+from requests.cookies import RequestsCookieJar
 
 from bugwarrior.config import asbool, die
 from bugwarrior.services import IssueService, Issue
+
+class ObliviousCookieJar(RequestsCookieJar):
+    def set_cookie(self, *args, **kwargs):
+        """Simply ignore any request to set a cookie."""
+        pass
+
+    def copy(self):
+        """Make sure to return an instance of the correct class on copying."""
+        return ObliviousCookieJar()
+
+
+class JIRA(BaseJIRA):
+    def _create_http_basic_session(self, username, password):
+        super(JIRA, self)._create_http_basic_session(username, password)
+
+        # XXX: JIRA logs the web user out if we send the session cookies we get
+        # back from the first request in any subsequent requests. As we don't
+        # need cookies when accessing the API anyway, just ignore all of them.
+        self._session.cookies = ObliviousCookieJar()
+
+    def close(self):
+        self._session.close()
 
 
 class JiraIssue(Issue):
